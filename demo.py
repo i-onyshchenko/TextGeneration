1015c1d6f80a7f9c63b1fa2f371921d36ae4fe38
ihor_acer <igorko323@gmail.com>
2019-11-25 10:48:13 +0200

    add prompt

--- demo.py
+++ demo.py
@@ -2,7 +2,23 @@ from generation import TextGenerator
 from grammar import GrammarChecker
 
 
-def main():
+def demo_console():
+    prompt = input("Enter your text >>")
+
+    print("Your text: ", prompt)
+    grammar_checker = GrammarChecker()
+    length = 20
+
+    models = ['gpt2', 'gpt2-medium', 'gpt2-large']
+
+    for model in models:
+        gpt2 = TextGenerator(model_name_or_path=model)
+        output_text = gpt2.generate(prompt, length=length)
+        print(model + '\n' + output_text + '\n')
+        grammar_checker.check(output_text)
+
+
+def demo_file():
     # tale = "Once upon a time there were Three Bears, who lived together in a house of their own, in a wood. One of them was a Little, Small Wee Bear; and one was a Middle-sized Bear, and the other was a Great, Huge Bear. They had each a pot for their porridge, a little pot for the Little, Small, Wee Bear; and a middle-sized pot for the Middle Bear, and a great pot for the Great, Huge Bear. And they had each a chair to sit in; a little chair for the Little, Small, Wee Bear; and a middle-sized chair for the Middle Bear; and a great chair for the Great, Huge Bear. And they had each a bed to sleep in; a little bed for the Little, Small, Wee Bear; and a middle-sized bed for the Middle Bear; and a great bed for the Great, Huge Bear."
     tale = "Once upon a time down on an old farm, lived a duck family, and Mother Duck had been sitting on a clutch of new eggs. One nice morning, the eggs hatched and out popped six chirpy ducklings. But one egg was bigger than the rest, and it didn't hatch. Mother Duck couldn't recall laying that seventh egg. How did it get there? TOCK! TOCK! The little prisoner was pecking inside his shell."
     article = "The core idea behind the Transformer model is self-attention—the ability to attend to different positions of the input sequence to compute a representation of that sequence. Transformer creates stacks of self-attention layers and is explained below in the sections Scaled dot product attention and Multi-head attention."
@@ -55,4 +71,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    demo_console()
